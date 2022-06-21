170df84a08004b2a5325e073bb7a3db91d937d85
honor-sky <70474085+honor-sky@users.noreply.github.com>
2022-06-21 20:12:21 +0900

    update "app_urls.html"

--- mysite/gangmini_hompy/urls.py
+++ mysite/gangmini_hompy/urls.py
@@ -7,5 +7,5 @@ app_name="gangmini_hompy"
 urlpatterns = [
     path('', views.index, name='index'),
     path('gangmini_hompy/project.html', views.project, name='project'),
-    path('gangmini_hompy/study.html', views.index, name='study'),
+    path('gangmini_hompy/study.html', views.study, name='study'),
 ]
